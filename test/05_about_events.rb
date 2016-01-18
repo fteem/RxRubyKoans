f64db51c557d53a59e7d228ac41ea6dd47a006e9
Ile Eftimov <ileeftimov@gmail.com>
2016-01-18 02:52:42 +0100

    feat: 05 - about events

---
+++ test/05_about_events.rb
@@ -0,0 +1,33 @@
+require_relative 'test_helper'
+require 'event_emitter'
+
+class FooEmitter
+  include EventEmitter
+end
+
+class AboutEvents < Minitest::Test
+  def test_the_main_event
+    skip "Event wrapping not available, yet."
+
+    received = []
+    e = FooEmitter.new
+
+    subscription = RxRuby::Observable
+                    .from_event(e, 'change')
+                    .subscribe {|r| received.push(r) }
+
+    e.emit('change', 'R')
+    e.emit('change', 'x')
+    e.emit('change', 'R')
+    e.emit('change', 'u')
+    e.emit('change', 'b')
+    e.emit('change', 'y')
+
+    subscription.dispose
+
+    e.emit('change', '!')
+
+    assert_equal "RxRuby", received.join('')
+  end
+
+end
