a92df30cdd601e3f5447905cc33547cd3172647a
Ile Eftimov <ileeftimov@gmail.com>
2016-01-18 01:32:15 +0100

    first koans

---
+++ README.md
@@ -0,0 +1,83 @@
+
+# Learn the Reactive Extensions for Ruby (RxRuby) with RxRuby Koans #
+
+The Koans walk you along the path to enlightenment in order to learn [RxRuby](https://github.com/Reactive-Extensions/RxRuby). The goal is to learn the RxRuby library. We also teach you culture. Testing is not just something we pay lip service to, but something we live. It is essential in your quest to learn and do great things in the library.
+
+## Structure ##
+
+The koans are broken out into areas by file, events are covered in about_events.rb, time is introduced in about_time.rb, etc. They are presented in order in the index.rb file.
+
+Each koan builds up your knowledge of RxRuby and builds upon itself.
+
+Some koans simply need to have the correct answer substituted for an incorrect one. Some, however, require you to supply your own answer. If you see the method __ (a double underscore) listed, it is a hint to you to supply your own code in order to make it work correctly.
+
+## Installing Node.js ##
+
+In order to run the koans you need Node.js installed. If you do not already have Node.js set up, please visit the [Node.js](http://nodejs.org/) site to install Node.js.
+
+To verify your installation, in your terminal window simply type:
+```bash
+$ node --version
+```
+Any response for Node with a version number greater than 0.10.x is fine.
+
+## Installing the RxRuby Koans ##
+
+To install the RxRuby Koans, download via Git and add the depdencies via NPM:
+```bash
+$ git clone https://github.com/Reactive-Extensions/RxRubyKoans.git
+$ cd RxRubyKoans
+[RxRubyKoans ]$ npm install
+```
+
+## The Path to Enlightenment ##
+
+You can run the tests by calling `npm test` at your terminal window.
+```bash
+[RxRubyKoans] $ npm test
+```
+
+## Red, Green, Refactor ##
+
+In test-driven development (TDD) the mantra has always been red: write a failing test and run it, green: make the test pass, and refactor: look at the code and see if you can make it any better.
+
+With the koans, you will need to run the tests and see it fail (red), make the test pass (green), then take a moment and reflect upon the test to see what it is teaching you and improve the code to better communicate its intent (refactor).
+
+The very first time you run it you will see the following output:
+```bash
+  Observable Streams
+    1) simple subscription
+
+  1) Observable Streams simple subscription:
+     Error: expected 42 to equal undefined
+```
+
+You have come to the first error. You then open up the `about_streams.js` file in your text editor and look at `simple subscription`:
+
+```js
+test('simple subscription', function () {
+  Observable.just(42).subscribe(function (x) { equal(x, __); });
+});
+```
+
+You then change `__` to `42` and run the tests again. You should now have fixed the error.
+
+Before moving on, think about what you are learning.
+
+In this specific case, ignore everything except the method name  and the parts inside the method. The goal is for you to see that if you pass a value to the proper assertion method method, it will either ensure it is true and continue on, or fail if in fact the statement is false.
+
+## License ##
+
+Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you
+may not use this file except in compliance with the License. You may
+obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing permissions
+and limitations under the License.
---
+++ ruby/01_about_streams.rb
@@ -0,0 +1,160 @@
+require 'minitest/autorun'
+require 'rx_ruby'
+
+class AboutStreams < Minitest::Test
+  def test_simple_subscription
+    stream = RxRuby::Observable.just(42)
+    stream.subscribe {|x| assert_equal(42, x) }
+  end
+
+  def test_what_comes_in_goes_out
+    stream = RxRuby::Observable.just(101)
+    stream.subscribe {|x| assert_equal(x, 101) }
+  end
+
+  def test_same_as_event_stream
+    events = RxRuby::Subject.new
+    source = events.as_observable
+    source.subscribe {|x| assert_equal(37, x) }
+    events.on_next(37)
+  end
+
+  def test_how_event_relate_to_observables
+    observable_result = 1
+    RxRuby::Observable.just(73).subscribe {|x| observable_result = x }
+
+    event_stream_result = 1
+    events = RxRuby::Subject.new
+    source = events.as_observable
+    source.subscribe {|x| event_stream_result = x }
+    events.on_next(73)
+
+    assert_equal observable_result, event_stream_result
+  end
+
+  def test_event_stream_have_multiple_results
+    event_stream_result = 0
+    events = RxRuby::Subject.new
+    source = events.as_observable
+    subscriber = source.subscribe {|x| event_stream_result += x }
+
+    events.on_next(10)
+    events.on_next(7)
+
+    assert_equal 17, event_stream_result
+  end
+
+  def test_simple_return
+    received = ''
+    RxRuby::Observable.just('foo').subscribe {|x| received = x }
+
+    assert_equal 'foo', received
+  end
+
+  def test_the_last_event
+    received = ''
+    names = %w[foo bar]
+
+    RxRuby::Observable.from(names).subscribe {|x| received = x }
+
+    assert_equal 'bar', received
+  end
+
+  def test_everything_counts
+    received = 0
+    numbers = [3,4]
+
+    RxRuby::Observable.from(numbers).subscribe {|x| received += x }
+
+    assert_equal 7, received
+  end
+
+  def test_this_is_still_an_event_stream
+    received = 0
+    numbers = RxRuby::Subject.new
+    source = numbers.as_observable
+
+    source.subscribe {|x| received += x }
+
+    numbers.on_next(10)
+    numbers.on_next(5)
+
+    assert_equal 15, received
+  end
+
+  def test_all_events_will_be_received
+    received = 'Working '
+    numbers = Array(5..9).reverse
+
+    RxRuby::Observable.from(numbers).subscribe {|x| received += x.to_s }
+
+    assert_equal 'Working 98765', received
+  end
+
+  def test_do_things_in_the_middle
+    status = []
+    range = Array(1..4).reverse
+    days_till_test = RxRuby::Observable.from(range)
+
+    # Erhm, is `do` the RxRuby's to RxJS's `tap` ?
+    days_till_test.do {|d| status.push("#{d} = #{d == 1 ? 'Study like mad' : 'Party'}") }.subscribe
+
+    assert_equal "4 = Party,3 = Party,2 = Party,1 = Study like mad", status.join(',')
+  end
+
+  def test_nothing_listens_until_you_subscribe
+    sum = 0
+    range = Array(1..10)
+    numbers = RxRuby::Observable.from(range)
+
+    observable = numbers.do {|x| sum += x }
+
+    assert_equal 0, sum
+
+    observable.subscribe
+    assert_equal 55, sum
+  end
+
+  def test_events_after_you_unsubscribe_dont_count
+    sum = 0
+    numbers = RxRuby::Subject.new
+
+    numbers_source = numbers.as_observable
+    observable = numbers_source.do {|n| sum += n}
+    subscription = observable.subscribe
+
+    numbers.on_next(1)
+    numbers.on_next(2)
+
+    subscription.dispose
+
+    numbers.on_next(3)
+    numbers.on_next(4)
+
+    assert_equal 3, sum
+  end
+
+  def test_events_while_subscribing
+    received = []
+    words = RxRuby::Subject.new
+    words_source = words.as_observable
+
+    observable = words_source.do {|x| received.push(x) }
+
+    words.on_next('Peter')
+    words.on_next('said')
+
+    subscription = observable.subscribe
+
+    words.on_next('you')
+    words.on_next('look')
+    words.on_next('pretty')
+
+    subscription.dispose
+
+    words.on_next('ugly')
+
+    assert_equal 'you look pretty', received.join(' ')
+  end
+
+end
